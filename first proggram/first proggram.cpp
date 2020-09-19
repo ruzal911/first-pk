928d3d0e4b07e27300ab01f3b80cd2c17357d8db
ruzal911 <Ruzal1212@yandex.ru>
2020-09-19 15:32:57 +0300

    Добавьте файлы проекта.

---
+++ first proggram/first proggram.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+
+void main()
+{
+	int const size = 10;
+	setlocale(LC_ALL, "RUS");
+	srand(time(NULL));
+	int a[size];
+	bool c;int b;
+	bc:
+	for (int i = 0; i < size;)
+	{
+		c = false;b = rand() % 20;
+		for (int j = 0; j < i; j++)
+		{
+			if (a[j] == b)
+			{
+				c = true;
+				break;
+			}
+		}
+
+		if (c == false)
+		{
+			a[i] = b;
+			i++;
+		}
+	}
+	for (int i = 0; i < size; i++)
+	{
+		cout << a[i] << endl;
+
+	}
+	cout << "Введите 1 если хотите продолжить или 0 если хотите завершить " << endl;
+	int m;
+	cin >> m;
+	if (m == 1)
+		goto bc;
+	
+}
---
+++ калькуляттор/калькуляттор.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+class Human
+{
+public:
+
+	int age;
+	string name;
+};
+int main()
+{
+	Human Firstname;
+	Firstname.age = 5;
+	Firstname.name = "Ruzal";
+	cout << Firstname.name << endl;
+	cout << Firstname.age << endl;
+
+	return 0;
+}
